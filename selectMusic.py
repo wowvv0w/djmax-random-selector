5bebd0390851f821920c466e01430cbb47461138
wowvv0w <67505068+wowvv0w@users.noreply.github.com>
2021-03-21 23:54:22 +0900

    delete genre,length in csv

--- selectMusic.py
+++ selectMusic.py
@@ -5,9 +5,9 @@ import time
 import keyboard as kb
 
 isnt_alphabet = lambda x: x not in ascii_letters
-name = ('Title', 'Artist', 'Genre', 'Series', '4BNM', '4BHD', '4BMX', '4BSC',
-    '5BNM', '5BHD', '5BMX', '5BSC', '6BNM', '6BHD', '6BMX', '6BSC',
-    '8BNM', '8BHD', '8BMX', '8BSC', 'Length')
+name = ('Title', 'Artist', 'Series',
+    '4BNM', '4BHD', '4BMX', '4BSC', '5BNM', '5BHD', '5BMX', '5BSC',
+    '6BNM', '6BHD', '6BMX', '6BSC', '8BNM', '8BHD', '8BMX', '8BSC')
 _styles = ('NM', 'HD', 'MX', 'SC')
 
 # YourData 읽기
