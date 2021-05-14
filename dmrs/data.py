34316cf4c158e2798d61a214cfaaf8076f814f9a
wowvv0w <67505068+wowvv0w@users.noreply.github.com>
2021-05-15 01:11:43 +0900

    ready to test

--- dmrs/data.py
+++ dmrs/data.py
@@ -1,100 +1,110 @@
 import pandas as pd
-from requests import get, codes
+import requests
 
 
-_name = ('Title', 'Artist', 'Series',
+_column = ('Title', 'Artist', 'Series',
     '4BNM', '4BHD', '4BMX', '4BSC', '5BNM', '5BHD', '5BMX', '5BSC',
     '6BNM', '6BHD', '6BMX', '6BSC', '8BNM', '8BHD', '8BMX', '8BSC')
 
-ALL_TRACK_DATA = './AllTrackData.csv'
-TEST_DATA = './test_data.csv'
 YOUR_DATA = './data/YourData.csv'
+TEST_DATA = './test_data.csv'
 
-ALL_TRACK_DATA_URL = 'https://raw.githubusercontent.com/wowvv0w/DJMAX_Random_Selector/main/AllTrackData.csv'
+DATABASE_URL = 'https://raw.githubusercontent.com/wowvv0w/DJMAX_Random_Selector/main/AllTrackData.csv'
 
 def read_data(test):
     """
-    Return csv file created from "AllTrackData.csv".
+    Return dataframe.
 
     - `test` defines if this run is for test or not.
 
-    If `test` is True, return "test_data.csv". Otherwise, return "data\\YourData.csv".
+    If `test` is True, return `TEST_DATA`. Otherwise, return `YOUR_DATA`.
     """
 
-    if test:
-        data = pd.read_csv(TEST_DATA, names = _name)
+    if not test:
+        data = pd.read_csv(YOUR_DATA, names = _column)
     else:
-        data = pd.read_csv(YOUR_DATA, names = _name)
+        data = pd.read_csv(TEST_DATA, names = _column)
 
     return data
 
-def edit_data(series, test):
+def edit_data(series_filter, test):
     """
     Edit YourData.csv
     """
 
-    data = pd.read_csv(ALL_TRACK_DATA, names = _name)
-
-    filtered = data[data['Series'].isin(series)]
-
-    filtered = _filter_special_music(filtered, series)
-
-    if test:
-        filtered.to_csv(TEST_DATA, index=None, header=None)
+    if not test:
+        db = YOUR_DATA
     else:
-        filtered.to_csv(YOUR_DATA, index=None, header=None)
+        db = TEST_DATA
+    
+    success = _get_database(db)
+    if not success:
+        return
+    title_filter = _generate_title_filter(series_filter)
 
-def _filter_special_music(df, series):
+    data = pd.read_csv(db)
+    filtered = data.values.tolist()
+    filtered = [
+        row for row in filtered
+        if row[0] not in title_filter and row[2] in series_filter
+        ]
+    data = pd.DataFrame(filtered, columns=_column)
+
+    data.to_csv(db, index=None, header=None)
+
+def _generate_title_filter(series):
     """
-    Exclude music which has complicated condition
+    Generates title filter.
     """
 
+    title_filter = set()
     # RP in DLC
     if 'P3' not in series:
-        df = df.drop(df[df['Title'] == 'glory day (Mintorment Remix)'].index)
-        df = df.drop(df[df['Title'] == 'glory day -JHS Remix-'].index)
+        title_filter.add('glory day (Mintorment Remix)')
+        title_filter.add('glory day -JHS Remix-')
     if 'TR' not in series:
-        df = df.drop(df[df['Title'] == 'Nevermind'].index)
+        title_filter.add('Nevermind')
     if 'CE' not in series:
-        df = df.drop(df[df['Title'] == 'Rising The Sonic'].index)
+        title_filter.add('Rising The Sonic')
     if 'BS' not in series:
-        df = df.drop(df[df['Title'] == 'ANALYS'].index)
+        title_filter.add('ANALYS')
     if 'T1' not in series:
-        df = df.drop(df[df['Title'] == 'Do you want it'].index)
+        title_filter.add('Do you want it')
     if 'T2' not in series:
-        df = df.drop(df[df['Title'] == 'End of Mythology'].index)
+        title_filter.add('End of Mythology')
     if 'T3' not in series:
-        df = df.drop(df[df['Title'] == 'ALiCE'].index)
+        title_filter.add('ALiCE')
 
     # Link Disc
     if 'CE' in series and 'BS' not in series and 'T1' not in series:
-        df = df.drop(df[df['Title'] == 'Here in the Moment ~Extended Mix~'].index)
+        title_filter.add('Here in the Moment ~Extended Mix~')
     if 'CE' not in series and 'BS' in series and 'T1' not in series:
-        df = df.drop(df[df['Title'] == 'Airwave ~Extended Mix~'].index)
+        title_filter.add('Airwave ~Extended Mix~')
     if 'CE' not in series and 'BS' not in series and 'T1' in series:
-        df = df.drop(df[df['Title'] == 'SON OF SUN ~Extended Mix~'].index)
+        title_filter.add('SON OF SUN ~Extended Mix~')
 
-    return df
+    return title_filter
 
-def _update_database():
+def _get_database(database):
     """
-    Updates AllTrackData.csv from remote repository.
+    Gets database from remote repository.
     """
 
     try:
-        response = get(ALL_TRACK_DATA_URL)
+        response = requests.get(DATABASE_URL)
     except:
         print('failed')
         return
 
-    if response.status_code == codes.ok:
-        with open('test1.csv', 'w', encoding='UTF-8') as file:
+    if response.status_code == requests.codes.ok:
+        with open(database, 'w', encoding='UTF-8') as file:
             file.write(response.text)
         print('updated')
+        return True
     else:
         print('there is something wrong')
-
-
+        return
 
 if __name__ == '__main__':
-    _update_database()
+    ex_series = {'RP', 'P1', 'P2', 'GG'}
+    edit_data(ex_series, True)
